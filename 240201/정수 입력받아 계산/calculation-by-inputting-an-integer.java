6215fedab04cd9619a6014c0adba4c56d836d370
Kim Juwon <59572441+jw0111@users.noreply.github.com>
2024-02-01 19:11:51 +0900

    Title: 정수 입력받아 계산, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240201/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        System.out.println(n*2+3);
+    }
+}
