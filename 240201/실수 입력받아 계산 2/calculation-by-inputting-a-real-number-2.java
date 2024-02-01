e442a00f21dee3650026772fee6fed4b94fd67a8
Kim Juwon <59572441+jw0111@users.noreply.github.com>
2024-02-01 19:22:46 +0900

    Title: 실수 입력받아 계산 2, Time: 116ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240201/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        double a = Double.parseDouble(st.nextToken());
+        a += 1.5;
+        System.out.println(String.format("%.2f", a));
+    }
+}
