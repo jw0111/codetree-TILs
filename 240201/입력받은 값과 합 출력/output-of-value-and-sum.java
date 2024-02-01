52e5102abb5d02ae70323199e393570d5e620dc5
Kim Juwon <59572441+jw0111@users.noreply.github.com>
2024-02-01 19:28:22 +0900

    Title: 입력받은 값과 합 출력, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240201/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(String.format("%d %d %d", a, b, a+b));
+    }
+}
