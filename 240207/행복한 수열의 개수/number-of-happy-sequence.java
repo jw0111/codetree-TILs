7c7775fd27d3daf38acb401234ea66dd045efee3
Kim Juwon <59572441+jw0111@users.noreply.github.com>
2024-02-07 20:00:38 +0900

    Title: 행복한 수열의 개수, Time: 87ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240207/행복한 수열의 개수/number-of-happy-sequence.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int N, M;
+    static int ans = 0;
+    static int[][] board = new int[101][101];
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N; j++){
+               int tmp = Integer.parseInt(st.nextToken()); 
+               board[i][j] = tmp;
+            }
+        }
+
+        for(int i = 0; i < N; i++){
+            int cnt = 1;
+
+            for(int j = 1; j < N; j++){
+                if(board[i][j - 1] == board[i][j]){
+                    cnt++;
+                    if(cnt == M){
+                        ans++;
+                        break;
+                    }
+                }
+                else
+                    cnt = 1;
+            }
+        }
+
+        for(int i = 0; i < N; i++){
+            int cnt = 1;
+            for(int j = 1; j < N; j++){
+                if(board[j - 1][i] == board[j][i]) {
+                    cnt++;
+                    if(cnt == M){
+                        ans++;
+                        break;
+                    }
+                }
+                else
+                    cnt = 1;
+            }
+        }
+
+        System.out.println(ans);
+    }
+}
