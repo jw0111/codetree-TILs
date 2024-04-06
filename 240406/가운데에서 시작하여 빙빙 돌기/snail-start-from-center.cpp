d8f8423df442a9a12ce310cce6ba02543a3cbfe4
Kim Juwon <59572441+jw0111@users.noreply.github.com>
2024-04-06 14:27:43 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240406/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
+++ 240406/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -6,7 +6,7 @@ int dy[4] = {1, 0, -1, 0};
 int n;
 int board[101][101];
 int x, y;
-int dir = 0;
+int dir = 3;
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -15,16 +15,19 @@ int main() {
 
     for(int i = 1; i <= n * n; i++){
         board[x][y] = i;
-        int nx = x + dx[dir];
-        int ny = y + dy[dir];
+        int tmpDir = (dir + 1) % 4;
+        int nx = x + dx[tmpDir];
+        int ny = y + dy[tmpDir];
         if(nx < 0 || nx >= n || ny < 0 || ny >= n || board[nx][ny] != 0){
-            dir = (dir + 1) % 4;
             nx = x + dx[dir];
             ny = y + dy[dir];
-
+        }
+        else {
+            dir = tmpDir;
         }
         x = nx;
         y = ny;
+    
     }
 
     for(int i = 0; i < n; i++){
