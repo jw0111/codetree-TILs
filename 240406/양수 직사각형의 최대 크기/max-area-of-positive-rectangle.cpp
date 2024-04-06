c158a6d731a310774f5ec0426c04726e845a552b
Kim Juwon <59572441+jw0111@users.noreply.github.com>
2024-04-06 17:01:22 +0900

    Title: 양수 직사각형의 최대 크기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240406/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int board[21][21];
+int n, m;
+
+bool checksquare(int i, int j, int l, int k){
+    for(int a = i; a <= l; a++){
+        for(int b = j; b <= k; b++){
+            if(board[a][b] < 0)
+                return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    int ans = 0;
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> board[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            for(int k = i; k < n; k++){
+                for(int l = j; l < m; l++){
+                    if(checksquare(i, j, k, l)){
+                        int cnt = (k + 1 - i) * (l + 1 - j);
+                        //cout << i << " " << j << " " << k << " " << l << " " << cnt <<'\n';
+                        ans = max(ans, cnt);
+                    }
+                }
+            }
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
