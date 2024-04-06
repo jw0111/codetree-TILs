f05b03de620969d9f3959d9ef24f5ab7482b0e4c
Kim Juwon <59572441+jw0111@users.noreply.github.com>
2024-04-06 19:47:07 +0900

    Title: 2차원 바람, Time: 12ms, Memory: 0MB, Status: Passed - CodeTree

--- 240406/2차원 바람/The-2D-wind-blows.cpp
+++ 240406/2차원 바람/The-2D-wind-blows.cpp
@@ -8,6 +8,22 @@ int r1, c1, r2, c2;
 int dx[4] = {-1, 1, 0, 0};
 int dy[4] = {0, 0, -1, 1};
 
+void rotate(int r1, int c1, int r2, int c2) {
+    int temp = board[r1][c1];
+    for(int row = r1; row < r2; row++)
+        board[row][c1] = board[row + 1][c1];
+    
+    for(int col = c1; col < c2; col++)
+        board[r2][col] = board[r2][col + 1];
+
+    for(int row = r2; row > r1; row--)
+        board[row][c2] = board[row - 1][c2];
+    
+    for(int col = c2; col > c1; col--)
+        board[r1][col] = board[r1][col - 1];
+    board[r1][c1 + 1] = temp;
+}
+
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -20,28 +36,7 @@ int main() {
 
     for(int time = 0; time < Q; time++){
         cin >> r1 >> c1 >> r2 >> c2;
-        int tmp1 = board[r1 + 1][c1];
-        int tmp2 = board[r2][c2];
-        int tmp3 = board[r2][c1];
-        int tmp4 = board[r1][c2];
-
-        for(int i = c2; i > c1; i--){
-            board[r1][i] = board[r1][i - 1];
-        }
-        for(int i = r2; i > r1; i--){
-            board[i][c2] = board[i - 1][c2];
-        }
-        board[r1 + 1][c2] = tmp4;
-
-        for(int i = c1; i < c2; i++){
-            board[r2][i] = board[r2][i + 1];
-        }
-        board[r2][c2 - 1] = tmp2;
-        board[r2 - 1][c1] = tmp3;
-        for(int i = r1; i < r2 - 1; i++){
-            board[i][c1] = board[i + 1][c1];
-        }
-        board[r1][c1] = tmp1; // 여기까지 회전
+        rotate(r1, c1, r2, c2);
 
         for(int i = r1; i <= r2; i++){
             for(int j = c1; j <= c2; j++){
