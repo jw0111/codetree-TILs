a9486e9d18b891a737ea411694de0c1331f8047d
Kim Juwon <59572441+jw0111@users.noreply.github.com>
2024-04-06 12:11:26 +0900

    Title: 되돌아오기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240406/되돌아오기/come-back.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+map<char, int> dirMap;
+int N;
+int x = 0, y = 0;
+int ans = 0;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    dirMap.insert({'N', 0});
+    dirMap.insert({'S', 1});
+    dirMap.insert({'W', 2});
+    dirMap.insert({'E', 3});
+
+    cin >> N;
+    for(int i = 0; i < N; i++){
+        char tmpDir;
+        int dist;
+        cin >> tmpDir >> dist;
+
+        int dir = dirMap.at(tmpDir);
+        for(int time = 0; time < dist; time++){
+            if(x == 0 && y == 0 && ans != 0){
+                cout << ans;
+                return 0;
+            }
+            x += dx[dir];
+            y += dy[dir];
+            ++ans;
+        }
+    }
+    cout << -1;
+    return 0;
+}
