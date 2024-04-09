a7a8c9365c0e13987ad11f79795fadd7e1d312f8
Kim Juwon <59572441+jw0111@users.noreply.github.com>
2024-04-09 21:57:39 +0900

    Title: 떨어지는 1자 블록, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240409/떨어지는 1자 블록/falling-horizontal-block.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int n, m, k;
+int board[101][101];
+int rowIdx = 0;
+
+void dropBlock(){
+    for(int i = 1; i <= n; i++){
+        for(int j = k; j <= m; j++){
+            if(board[i][j]){
+                rowIdx = i - 1;
+                return;
+            }
+        }
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m >> k;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cin >> board[i][j];
+        }
+    }
+
+    dropBlock();
+
+    for(int j = k; j <= k + m; j++){
+        board[rowIdx][j] = 1;
+    }
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            cout << board[i][j] << " ";
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
