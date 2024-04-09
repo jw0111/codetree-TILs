910c63e9d85d84d96a762686a86502d5b20abb19
Kim Juwon <59572441+jw0111@users.noreply.github.com>
2024-04-10 01:23:16 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240410/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.cpp
+++ 240410/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.cpp
@@ -9,7 +9,6 @@ int ans = 0;
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {1, 0, -1, 0}; // 시계방향(오, 아래, 왼, 위)
 int dir = 0;
-// 초기 위치로 돌아오면 탈출 불가로 간주
 
 void escape(){
     int curX = x;
@@ -18,9 +17,20 @@ void escape(){
         int nx = curX + dx[dir];
         int ny = curY + dy[dir];
         if(board[nx][ny] == '#'){ // 진행 방향에 벽이 있음
-            dir = (dir + 3) % 4; //반시계방향 회전
-            curX += dx[dir];
-            curY += dy[dir];
+            int cnt = 0;
+            while(board[nx][ny] != '.'){
+                dir = (dir + 3) % 4; //반시계방향 회전
+                nx = curX + dx[dir];
+                ny = curY + dy[dir];
+                cnt++;
+                if(cnt > 4){ // 계속 회전만 한다면 강제 종료
+                    ans = -1;
+                    return;
+                }    
+            }
+
+            // curX += dx[dir];
+            // curY += dy[dir];
         }
         else if(board[nx][ny] == '.'){ // 벽이 없음
             curX = nx;
@@ -32,13 +42,14 @@ void escape(){
         }
         // cout << curX << " " << curY << '\n';
         // cout << ans << "\n\n";
+
         if(nx <= 0 || nx > N || ny <= 0 || ny > N){
             //cout << "escaped\n";
             ++ans;
             return;
         }
         
-    }while(curX != x || curY != y);
+    }while(ans <= N * N);
     ans = -1;
     return;
 }
