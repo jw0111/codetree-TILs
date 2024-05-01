4b043d88696fe49a487f7d85e1a929150992485b
Kim Juwon <59572441+jw0111@users.noreply.github.com>
2024-05-01 16:28:28 +0900

    Title: 연속된 숫자 만들기 3, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240501/연속된 숫자 만들기 3/create-consecutive-numbers-3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int a, b, c;
+    static int answer = -1;
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        if(a + 1 == b && b + 1 == c)
+            answer = 0;
+        else if(a + 2 == b && b + 2 == c)
+            answer = 1;
+        else answer = 3;
+
+        System.out.print(answer);
+    }
+}
