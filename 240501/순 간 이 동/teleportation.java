e0c9a115eb77c235605e49f3906c7282cf0471c7
Kim Juwon <59572441+jw0111@users.noreply.github.com>
2024-05-01 15:02:48 +0900

    Title: 순 간 이 동, Time: 89ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240501/순 간 이 동/teleportation.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+    static int A, B, x, y;
+    static int answer = Integer.MAX_VALUE;
+    public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
+        y = Integer.parseInt(st.nextToken());
+
+        // 1. 텔레포트 사용 X
+        answer = B - A;
+
+        // 2. x에서 y로 텔레포트
+        answer = Math.min(answer, Math.abs(x - A) + Math.abs(y - B));
+
+        // 3. y에서 x로 텔레퐅,
+        answer = Math.min(answer, Math.abs(x - B) + Math.abs(y - A));
+
+        System.out.print(answer);
+    }
+}
