e3c41aeacc894947a9b04a1d40f6aa47eb876a02
Kim Juwon <59572441+jw0111@users.noreply.github.com>
2024-05-03 19:06:41 +0900

    Title: 10진수로 변환하기, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240503/10진수로 변환하기/convert-to-decimal.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        // 여기에 코드를 작성해주세요.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String binary = br.readLine();
+        System.out.print(Integer.parseInt(binary, 2));
+    }
+}
